0a146f6292d33e1aecb3772b8d978271a5d542f0
Jakub Czerwiński <kubaczerwinski77@gmail.com>
2023-03-12 17:35:33 +0100

    Credits

--- healthy-eating/js/index.js
+++ healthy-eating/js/index.js
@@ -1,24 +1,20 @@
 var Animation = function ({ offset } = { offset: 10 }) {
   var _elements;
 
-  // Define a dobra superior, inferior e laterais da tela
   var windowTop = (offset * window.innerHeight) / 100;
   var windowBottom = window.innerHeight - windowTop;
   var windowLeft = 0;
   var windowRight = window.innerWidth;
 
   function start(element) {
-    // Seta os atributos customizados
     element.style.animationDelay = element.dataset.animationDelay;
     element.style.animationDuration = element.dataset.animationDuration;
-    // Inicia a animacao setando a classe da animacao
+
     element.classList.add(element.dataset.animation);
-    // Seta o elemento como animado
     element.dataset.animated = "true";
   }
 
   function isElementOnScreen(element) {
-    // Obtem o boundingbox do elemento
     var elementRect = element.getBoundingClientRect();
     var elementTop =
       elementRect.top + parseInt(element.dataset.animationOffset) ||
@@ -29,7 +25,6 @@ var Animation = function ({ offset } = { offset: 10 }) {
     var elementLeft = elementRect.left;
     var elementRight = elementRect.right;
 
-    // Verifica se o elemento esta na tela
     return (
       elementTop <= windowBottom &&
       elementBottom >= windowTop &&
@@ -38,17 +33,13 @@ var Animation = function ({ offset } = { offset: 10 }) {
     );
   }
 
-  // Percorre o array de elementos, verifica se o elemento está na tela e inicia animação
   function checkElementsOnScreen(els = _elements) {
     for (var i = 0, len = els.length; i < len; i++) {
-      // Passa para o proximo laço se o elemento ja estiver animado
       if (els[i].dataset.animated) continue;
-
       isElementOnScreen(els[i]) && start(els[i]);
     }
   }
 
-  // Atualiza a lista de elementos a serem animados
   function update() {
     _elements = document.querySelectorAll(
       "[data-animation]:not([data-animated])"
@@ -56,7 +47,6 @@ var Animation = function ({ offset } = { offset: 10 }) {
     checkElementsOnScreen(_elements);
   }
 
-  // Inicia os eventos
   window.addEventListener("load", update, false);
   window.addEventListener("scroll", () => checkElementsOnScreen(_elements), {
     passive: true,
@@ -67,7 +57,6 @@ var Animation = function ({ offset } = { offset: 10 }) {
     false
   );
 
-  // Retorna funcoes publicas
   return {
     start,
     isElementOnScreen,
@@ -75,8 +64,6 @@ var Animation = function ({ offset } = { offset: 10 }) {
   };
 };
 
-// Initialize
-var options = {
-  offset: 20, //percentage of window
-};
-var animation = new Animation(options);
+const animation = new Animation({
+  offset: 30,
+});
