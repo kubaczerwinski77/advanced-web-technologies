62c5384bf13c9da3bcfe45ac1a40739aceb1a004
Karolina936 <75604545+Karolina936@users.noreply.github.com>
2023-04-02 12:16:28 +0200

    delete books

--- books/books/src/main/java/pl/edu/pwr/ztw/books/validators/AuthorValidator.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.pwr.ztw.books.validators;
-
-public class AuthorValidator {
-    public static boolean isInvalidAuthor(String firstName, String lastName, int age) {
-        return firstName.equals("") || lastName.equals("") || age < 0;
-    }
-}
--- books/books/src/main/java/pl/edu/pwr/ztw/books/validators/BookValidator.java
+++
@@ -1,8 +0,0 @@
-package pl.edu.pwr.ztw.books.validators;
-
-public class BookValidator {
-
-    public static boolean isValidBook(String title, Integer pages, Integer authorId) {
-        return title.equals("") || pages <= 0 || authorId <= 0;
-    }
-}
